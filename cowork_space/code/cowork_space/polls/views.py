547758779b2e12dc56d45085b3f40e77d724b3b2
Edward Lui <silui@ucsc.edu>
2019-02-05 10:21:15 -0800

    Update views.py

--- cowork_space/code/cowork_space/polls/views.py
+++ cowork_space/code/cowork_space/polls/views.py
@@ -22,5 +22,5 @@ def test(request,multipler=2):
         p=multiprocessing.Process(target=f,args=(1500,))
         jobs.append(p)
         p.start()
-    return HttpResponse("this is the test page"+doodoo)
+    return HttpResponse("Hello people from insight!!!  "+doodoo)
 
