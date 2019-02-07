577aba940ad1a2898d8966c8129c9247dfdaae30
Edward Lui <silui@ucsc.edu>
2019-02-06 22:43:53 -0800

    Update views.py

--- cowork_space/code/cowork_space/polls/views.py
+++ cowork_space/code/cowork_space/polls/views.py
@@ -13,7 +13,7 @@ def f(x):
 
 def index(request):
     return HttpResponse("Sixth code being deployed" + doodoo + 
-                        '<img src="https://www.accountingweb.com/sites/default/files/styles/banner/public/security_breach_weerapatkiatdumrong.jpg?itok=nczbk1pC">'
+                        '<img src="https://www.dailydot.com/wp-content/uploads/534/ff/76995fe1e177a932-2048x1024.jpg">'
                        )
 
 def test(request,multipler=2):
